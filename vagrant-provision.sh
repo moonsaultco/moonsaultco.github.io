b109133bea8b2bbc9029393806883f79cf94f11e
Chad Baudoin <chadbaud@gmail.com>
2015-03-23 10:05:28 -0600

    user specific provisioning

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -3,31 +3,25 @@
 apt-get update
 apt-get install -y curl wget build-essential software-properties-common
 
-curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh \
-      | NVM_DIR=/usr/local/nvm bash
-
-chmod 2775 /usr/local/nvm
-
-source /usr/local/nvm/nvm.sh
-
-nvm install 0.10
-nvm use 0.10
-
-NVM_CMD="nvm use 0.10"
-if grep -Fxq "$NVM_CMD" my_list.txt
-then
-  echo "nvm installed"
-else
-  echo "source /usr/local/nvm/nvm.sh" >> /home/vagrant/.profile
-  echo $NVM_CMD >> /home/vagrant/.profile
-fi
-
-# node specific stuff
-npm install -g grunt-cli nodemon coffee-script
-
-# install rvm, this is a little fucked
+su vagrant <<'EOF'
+# rvm and ruby
 curl -sSL https://get.rvm.io | bash -s $1
 rvm install 2.2.1
 rvm use 2.2.1 --default
 
+# node
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | sh
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install 0.10
+nvm alias default 0.10
+
+# app specifc
 gem install bundler jekyll
+npm install -g grunt-cli nodemon coffee-script
+
+EOF
+
+echo "All done installing!
+
+Next steps: type 'vagrant ssh' to log into the machine."
