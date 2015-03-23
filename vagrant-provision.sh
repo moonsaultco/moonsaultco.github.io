98e1d1ccab6b4b07eeb201f6d9275cb5c582b5da
Chad Baudoin <chadbaud@gmail.com>
2015-03-23 10:12:25 -0600

    adding gemfile and adding comment

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -3,6 +3,7 @@
 apt-get update
 apt-get install -y curl wget build-essential software-properties-common
 
+# run as the vagrant user to get these installs to work correctly
 su vagrant <<'EOF'
 # rvm and ruby
 curl -sSL https://get.rvm.io | bash -s $1
@@ -19,7 +20,6 @@ nvm alias default 0.10
 # app specifc
 gem install bundler jekyll
 npm install -g grunt-cli nodemon coffee-script
-
 EOF
 
 echo "All done installing!
